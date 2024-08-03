89cb4066760924135fbd07e9379e47b090ebde39
saiteja <saiteja.chippagiri@jukshio.com>
2024-08-03 16:32:05 +0530

    Created structs to created tables in postgres using gorm

--- pkg/structures/db_structs.go
+++ pkg/structures/db_structs.go
@@ -1,9 +1,23 @@
 package structures
 
+import (
+	"time"
+)
+
 type DbConfig struct {
 	DB_USERNAME string
 	DB_PASSWORD string
 	DB_PORT     string
 	DB_HOSTNAME string
 	DB_NAME     string
+}
+
+type ProjectInfo struct {
+	ID          int       `gorm:"primaryKey;autoIncrement" json:"id"`
+	Name        string    `gorm:"not null" json:"name"`
+	Category    string    `gorm:"not null" json:"category"`
+	Description string    `gorm:"not null" json:"description"`
+	StartDate   time.Time `gorm:"not null;default:CURRENT_TIMESTAMP" json:"start_date"`
+	Status      string    `gorm:"not null" json:"status"`
+	EndDate     time.Time `gorm:"default:null" json:"end_date"`
 }
